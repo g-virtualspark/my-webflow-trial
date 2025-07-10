1a38199ed063e92ba9180bf7b93b3d860acef23d
g-virtualspark <159685357+g-virtualspark@users.noreply.github.com>
2025-07-10 11:23:36 +0800

    Fix next.config with correct basePath

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/pricing-calculator", // 👈 must match your Webflow Cloud mount path
 };
 
 export default nextConfig;
